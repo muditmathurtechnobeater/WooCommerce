6ca4fbc0afb53f5d08629f65a56e451c6b280810
muditmathurtechnobeater <70715176+muditmathurtechnobeater@users.noreply.github.com>
2020-09-03 22:14:22 +0530

    Add files via upload

---
+++ WooCommerceIntegration/WooCommerce/IMarketPlace.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WooCommerce
+{
+    interface IMarketPlace
+    {
+        bool ValidateCredentials(string shopUrl, string consumerKey, string consumerSecret);
+
+        void SyncOrders(string shopUrl, string consumerKey, string consumerSecret);
+
+        bool FulfillOrders(string shopUrl, string consumerKey, string consumerSecret, int orderId);
+    }
+}
